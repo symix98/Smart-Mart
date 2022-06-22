1d1dd441bfebd77016fe4700a68d43cd5959f649
symix98 <86252564+symix98@users.noreply.github.com>
2022-06-23 02:52:47 +0300

    dropdowns

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [NgbDropdownConfig]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  hotFoods: Array<any> = [
+    "Hot & Spicy Francheesie",
+    "Chorizo Potato & Green Chile Omelet",
+    "Spicy Southwest Bean & Corn Salad",
+    "Spicy Jalapeño & Bacon Flatbread",
+    "Spicy Grilled Salmon Kabobs",
+    "Chicken Paprikash Recipe",
+    "Spicy Chicken Lasagna Roll-Ups",
+  ];
+  snackFoods: Array<any> = [
+    "Pretzels",
+    "Salted peanuts",
+    "Potato chips",
+    "Popcorn",
+    "Trail mix",
+    "Indian Namkeen mixture",
+    "Choco Bar",
+  ];
+  Coffee: Array<any> = [
+    "Cafe Latte",
+    "Cafe Mocha",
+    "Americano",
+    "Flat White",
+    "Lungo",
+    "Macchiato",
+    "Affogato",
+  ];
+  iceCream: Array<any> = [
+    "Gelato",
+    "Kulfi",
+    "Sherbet",
+    "Sorbet",
+    "Frozen Yogurt",
+    "Soft Serve",
+    "Philadelphia Ice Cream",
+  ];
+  constructor(config: NgbDropdownConfig) {
+    // customize default values of dropdowns used by this component tree
+    config.placement = 'top-start';
+    config.autoClose = false;
+  }
 
   ngOnInit(): void {
   }
+
 
 }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderRoutingModule } from './header-routing.module';
 import { HeaderComponent } from './header.component';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
     CommonModule,
-    HeaderRoutingModule
+    HeaderRoutingModule,
+    NgbModule,
+    FormsModule,
+    MatSelectModule
   ]
 })
 export class HeaderModule { }
